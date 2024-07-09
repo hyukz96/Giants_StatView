46105218675f3d9a313425196f5f0c6a1acbc29e
Jung Hyuk Jin <richard960602@gmail.com>
2024-07-09 13:07:19 +0900

    "header.html" failed modulizing header html. Putting header html codes on every subpage htmls.

---
+++ www/common/header/js/header.js
@@ -0,0 +1,30 @@
+//header.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("toggle");
+    const navMove = document.querySelector('.mobile_nav');
+
+    if (menuToggle && navMove) {
+        navMove.style.top = navMove.style.top || "-500%";
+
+        menuToggle.addEventListener('click', function() {
+            if (navMove.style.top === "-500%") {
+                navMove.style.top = "100%";
+            } else {
+                navMove.style.top = "-500%";
+            }
+        });
+    }
+
+    const highLight = document.querySelectorAll('.nav-wrap ul li span');
+
+    highLight.forEach(span => {
+        span.parentElement.addEventListener('mouseover', function() {
+            span.style.display = "inline";
+        });
+
+        span.parentElement.addEventListener('mouseout', function() {
+            span.style.display = "none";
+        });
+    });
+});
---
+++ www/subpage/firstteam/js/firstteam.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#headers").load("/common/header/header.html");
+    $("#pitcherEntry").load("/common/entry_html/pitcher_entry.html");
+    $("#catcherEntry").load("/common/entry_html/catcher_entry.html");
+    $("#infielderEntry").load("/common/entry_html/infielder_entry.html");
+    $("#outfielderEntry").load("/common/entry_html/outfielder_entry.html");
+});
