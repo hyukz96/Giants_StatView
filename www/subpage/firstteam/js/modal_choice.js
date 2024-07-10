c3f7a8ae292d0f20da88f0d41525a70acf58c39f
Jung Hyuk Jin <richard960602@gmail.com>
2024-07-10 16:25:12 +0900

    "modal_choice" Done producing scripts.

--- www/subpage/firstteam/js/modal_choice.js
+++ www/subpage/firstteam/js/modal_choice.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
     $("#modalChoice").load("../../common/pop_ups/modal-choice/modal-choice.html", function(){
         const choiceModal = document.getElementById("choice");
-        const clickPlayer = document.querySelector('.player_info');
+        const playerInfos = document.querySelectorAll('.player_info');
 
-        clickPlayer.addEventListener("click", function(){
-            if (choiceModal) {
-                choiceModal.style.display = "none";
-            }
-
-            if (choiceModal.style.display === "none") {
-                choiceModal.style.display = "block"
-            } else {
-                choiceModal.style.display = "none";
-            }
+        playerInfos.forEach(player => {
+            player.addEventListener("click", function(){
+                if (choiceModal) {
+                    choiceModal.style.display = "none";
+                }
+    
+                if (choiceModal.style.display === "none") {
+                    choiceModal.style.display = "block"
+                } else {
+                    choiceModal.style.display = "none";
+                }
+            });
         });
+        
         
         const closeChoice = document.getElementById("ChoiceClose");
         closeChoice.addEventListener("click", function(){
